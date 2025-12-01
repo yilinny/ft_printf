5468cd9e74fbfc1965f43ff1e7e5f40740cdbcd0
Yi Lin Tan <yilintan47@gmail.com>
2025-12-01 10:18:01 +0800

    submission 3

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yi-ltan <yi-ltan@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 13:08:19 by yi-ltan           #+#    #+#             */
-/*   Updated: 2025/11/24 12:07:25 by yi-ltan          ###   ########.fr       */
+/*   Updated: 2025/12/01 10:12:51 by yi-ltan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,4 +63,12 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst,
+			void (*del)(void *));
+void	ft_lstdelone(t_list *lst,
+			void (*del)(void *));
+void	ft_lstiter(t_list *lst,
+			void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst,
+			void *(*f)(void *), void(*del)(void *));
 #endif
